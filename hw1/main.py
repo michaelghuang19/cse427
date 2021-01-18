a9f2e8f9847ea013987bd52df0a61bf4df948bcf
Michael Huang <mhuang19@cs.washington.edu>
2021-01-18 00:30:42 -0800

    initial copy of report

--- hw1/main.py
+++ hw1/main.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import numpy as np
 import csv
-import biopython
+import urllib
+
+# import Bio
 
 import fasta
 
-name_set = {MYOD1_HUMAN, TAL1_HUMAN, MYOD1_MOUSE, MYOD1_CHICK, MYODA_XENLA,
-MYOD1_DANRE, Q8IU24_BRABE, MYOD_DROME, LIN32_CAEEL, SYFM_HUMAN}
+name_set = {"MYOD1_HUMAN", "TAL1_HUMAN", "MYOD1_MOUSE", "MYOD1_CHICK", 
+"MYODA_XENLA", "MYOD1_DANRE", "Q8IU24_BRABE", "MYOD_DROME", "LIN32_CAEEL", 
+"SYFM_HUMAN"}
 
-accession_set = {P15172, P17542, P10085, P16075, P13904, Q90477, Q8IU24,
-P22816, Q10574, O95363}
-
-def main():
-  print ("hello world!")
-
-if __name__ == "__main__":
-  main()
+accession_set = {"P15172", "P17542", "P10085", "P16075", "P13904", "Q90477", 
+"Q8IU24", "P22816", "Q10574", "O95363"}
 
 def process_fasta(fasta):
   print(fasta)
 
+def get_fasta():
+  uniprot_url = "www.uniprot.org/uniprot/"
+  for item in accession_set:
+    print(uniprot_url + item + ".fasta");
+
+def main():
+  print ("hello world!")
+  get_fasta()
+
+if __name__ == "__main__":
+  main()
