dc67af8a9191150047d4a0d87c6ff071b4f3738d
Michael Huang <mhuang19@cs.washington.edu>
2021-02-12 00:43:08 -0800

    opened hw3

---
+++ hw3/main.py
@@ -0,0 +1,18 @@
+# Michael Huang (mhuang19)
+# 1862567
+
+import logomaker as lm
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from tabulate import tabulate
+
+import constants as c
+
+def main():
+  print("hello world")
+
+if __name__ == "__main__":
+  main()
