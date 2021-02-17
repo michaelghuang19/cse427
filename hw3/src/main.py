5e60ffcb6f029ed5aa435f6757052cdabeec7140
Michael Huang <mhuang19@cs.washington.edu>
2021-02-16 23:28:24 -0800

    TODO: get_hits

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -1,6 +1,7 @@
 # Michael Huang (mhuang19)
 # 1862567
 
+import itertools as it
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,8 +13,8 @@ import constants as c
 import data_structures as ds
 import helper as h
 
-def viterbi_training(sequence):
-  print("viterbi training")
+def viterbi(sequence):
+  print("performing viterbi algorithm")
   seq_len = len(sequence)
 
   assert seq_len > 0
@@ -28,29 +29,63 @@ def viterbi_training(sequence):
   # prob_list = viterbi trellis idea
   # state_list = actual states
 
-  prob_list[0][0] = c.transitions[0][0] + \
-    c.emissions[0][c.nucleotides.index(sequence[0])]
-  prob_list[1][0] = c.transitions[0][1] + \
-    c.emissions[1][c.nucleotides.index(sequence[0])]
+  prob_list[0][0] = transitions[0][0] + \
+    emissions[0][c.nucleotides.index(sequence[0])]
+  prob_list[1][0] = transitions[0][1] + \
+    emissions[1][c.nucleotides.index(sequence[0])]
 
   for i in range(c.n):
     print("---Viterbi Parameter Estimation: Trial {}---".format(i + 1))
 
-    for j, nuc in enumerate(sequence):
-      nuc = 0
+    for j in range(1, seq_len):
+      # print(tabulate(emissions))
+      # print(tabulate(transitions))
 
+      for k in range(len(state_list)):
 
-    print(c.init_emissions)
-    print(c.init_transitions)
+        prev_scores = prob_list[:, j - 1] + transitions[k]
+
+        state_list[k][j] = np.argmax(prev_scores)
+        prob_list[k][j] = emissions[k][c.nucleotides.index(
+            sequence[j])] + np.amax(prev_scores)
     
     # one test run
     break;
 
-def hmm_viterbi():
-  print("performing hmm viterbi algorithm")
+  path = traceback(prob_list, state_list)
+  print(path)
+  hit_list = get_hits(path)
 
-def traceback():
+def traceback(prob_list, state_list):
   print("doing traceback")
+  result = []
+  
+  last_index = np.argmax(prob_list[:, -1])
+  result.append(last_index)
+
+  # decrement to traceback
+  for i in range(len(state_list[0]) - 1, 0, -1):
+    last_index = int(state_list[last_index][i])
+    result.append(last_index)
+  
+  return np.flip(result)
+
+def get_hits(path):
+  print("finding hits")
+  result = []
+
+  # find 1's in path
+  # path = np.where(path == 1)
+  print(path)
+
+  # group by intervals where we had consecutive 1's
+
+  # for _, group in itertools.groupby(enumerate(hit_locations), key=lambda x: x[1]-x[0]):
+  #   group = list(group)
+  #   result.append((group[0][1], group[-1][1]))
+  #     self.intervals = hit_intervals
+
+  return result
 
 def test():
   print("test")
@@ -64,9 +99,7 @@ def main():
   seq = h.get_seq_list(fasta_list)[0]
 
   # remember to store log probabilities vs probabilities
-  viterbi_training(seq)
-
-  # test()
+  viterbi(seq)
 
   print("done")
 
